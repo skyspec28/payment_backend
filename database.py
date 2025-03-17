3bab7e00a2c97caca3bcc6b9196668ec2fe8bf6b
skyspec28 <maulomepelumi@gmail.com>
2025-03-17 12:37:02 +0100

    (fix): fixed db not connecting

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
-from sqlmodel import create_engine
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 
+SQLALCHEMY_DATABASE_URL ='postgresql://postgres:SpectruM@localhost/fastapi'
 
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-POSTGRES_DATABASE_URL ='postgresql://postgres:SpectruM@localhost/fastapi'
+SessionLocal=sessionmaker(autocommit=False ,autoflush=False , bind=engine)
 
-engine = create_engine(POSTGRES_DATABASE_URL, echo=True)
+Base =declarative_base()

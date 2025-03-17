82f140f1511f303b74d7fd31fc761a11e020775a
skyspec28 <maulomepelumi@gmail.com>
2025-03-17 12:40:06 +0100

    upated Schemas to include Usercreate

---
+++ schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel,EmailStr
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+class PostCreate(PostBase):
+    pass
+
+
+class Post(PostBase):
+    id:int
+
+class UserCreate(BaseModel):
+    email:EmailStr
+    password= str

ad1a521f4ea99ca5c2c702a4c40c2ea2846b503a
skyspec28 <maulomepelumi@gmail.com>
2025-03-18 13:58:49 +0100

    (feat):verification of access token added

--- oauth2.py
+++ oauth2.py
@@ -1,6 +1,6 @@
-from jose import JWTError ,jwt
+from jose import jwt
 from datetime import datetime, timedelta
-
+from . import schemas
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
@@ -13,3 +13,15 @@ def create_access_token(data:dict):
     encoded_jwt=jwt.encode(to_endcode,SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+def verify_access_token(token:str ,credentials_exception):
+
+    payload=jwt.decode(token,SECRET_KEY,ALGORITHM)
+
+    id=payload.get("user_id")
+
+    if id is None:
+        raise credentials_exception
+    token_data=schemas.UserResponse(id=id)
+
+    return token_data

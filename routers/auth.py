9921368cfad8a0c03efb63388563433c7223bcfc
skyspec28 <maulomepelumi@gmail.com>
2025-03-19 22:41:30 +0100

    (fix):Http exeception changed from 401 to 403

--- routers/auth.py
+++ routers/auth.py
@@ -1,22 +1,22 @@
 from fastapi import APIRouter ,Depends ,status ,HTTPException
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
-from .. import models ,database ,utils ,oauth2
+from .. import models ,database ,utils ,oauth2 ,schemas
 
 
 
 router=APIRouter()
 
 
-@router.post("/login",)
+@router.post("/login",response_model=schemas.Token)
 def login(user_credentials:OAuth2PasswordRequestForm=Depends() ,db:Session=Depends(database.get_db)):
     user=db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
-        raise HTTPException (status_code=status.HTTP_404_NOT_FOUND, detail="invalid credentials")
+        raise HTTPException (status_code=status.HTTP_403_FORBIDDEN, detail="invalid credentials")
     
     if not utils.verify(user_credentials.password , user.password):
-        raise HTTPException (status_code=status.HTTP_404_NOT_FOUND, detail="invalid credentials")
+        raise HTTPException (status_code=status.HTTP_403_FORBIDDEN, detail="invalid credentials")
     
     access_token=oauth2.create_access_token(data={"user_id":user.id})
     

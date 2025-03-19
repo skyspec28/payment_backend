44df4f49ecf2789b8ba40771fcc5082792873dd7
skyspec28 <maulomepelumi@gmail.com>
2025-03-19 22:39:10 +0100

    (feat):added get current user

--- oauth2.py
+++ oauth2.py
@@ -1,27 +1,48 @@
-from jose import jwt
-from datetime import datetime, timedelta
+from jose import jwt, JWTError
+from datetime import datetime, timedelta, timezone
 from . import schemas
+from fastapi import Depends, HTTPException, status
+from fastapi.security.oauth2 import OAuth2PasswordBearer
+
+# Define the OAuth2 scheme
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
+
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
-def create_access_token(data:dict):
-    to_endcode=data.copy()
-    exprie=datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_endcode.update({"exp":exprie})
-    encoded_jwt=jwt.encode(to_endcode,SECRET_KEY, algorithm=ALGORITHM)
-
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-def verify_access_token(token:str ,credentials_exception):
 
-    payload=jwt.decode(token,SECRET_KEY,ALGORITHM)
+def verify_access_token(token: str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        id:str = payload.get("user_id")
 
-    id=payload.get("user_id")
+        if id is None:
+            raise credentials_exception
 
-    if id is None:
+        token_data = schemas.TokenData(id=str(id))
+        
+    except JWTError:
         raise credentials_exception
-    token_data=schemas.UserResponse(id=id)
 
     return token_data
+
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    
+    return verify_access_token(token, credentials_exception)

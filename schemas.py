1165bc2b109e5a1f107335f8496e8581ef0707ba
skyspec28 <maulomepelumi@gmail.com>
2025-03-21 18:02:19 +0100

    (feat):Added owner to to Post

--- schemas.py
+++ schemas.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel,EmailStr
 
@@ -10,17 +11,27 @@ class PostBase(BaseModel):
 class PostCreate(PostBase):
     pass
 
+class UserResponse(BaseModel):
+    email:EmailStr
+    id:int
 
+    class Config:
+        from_attributes = True
+        
 class Post(PostBase):
     id:int
+    owner_id:int
+    owner:UserResponse
+
+    class Config:
+        from_attributes = True
+
 
 class UserCreate(BaseModel):
     email:EmailStr
     password: str
 
-class UserResponse(BaseModel):
-    email:EmailStr
-    id:int
+
 
 class LoginUser(BaseModel):
     email:EmailStr

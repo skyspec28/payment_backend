a3a139ef6458d64dbba451a279d728aba2e3bf81
skyspec28 <maulomepelumi@gmail.com>
2025-03-21 18:03:28 +0100

    (feat):moved secret key to enviroment varisbles

--- oauth2.py
+++ oauth2.py
@@ -3,13 +3,14 @@ from datetime import datetime, timedelta, timezone
 from . import schemas
 from fastapi import Depends, HTTPException, status
 from fastapi.security.oauth2 import OAuth2PasswordBearer
+from .config import settings
 
 # Define the OAuth2 scheme
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY =settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 
 def create_access_token(data: dict):

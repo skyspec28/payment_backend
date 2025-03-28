d9e415fd73e29114fea7dd621e2584b458dc8e83
skyspec28 <maulomepelumi@gmail.com>
2025-03-28 12:41:51 +0100

    (chore):clean up for code

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI 
 from . import models 
 from .database import engine
@@ -5,11 +6,20 @@ from .routers import post ,user ,auth ,votes
 from .config import settings
 
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
 
+origins=["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 
